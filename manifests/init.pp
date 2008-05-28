14b8256ebd126be2f45ed6a77e156c9d3c03c80a
Marcel Haerry <haerry@puzzle.ch>
2008-05-28 18:44:51 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# xinetd module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "xinetd": }
+class xinetd {}
