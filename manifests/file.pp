942e4691aba21cf43d060c0a57426539b38c58fd
mh <mh@immerda.ch>
2010-10-22 23:24:30 +0200

    add define to manage xinetd files

---
+++ manifests/file.pp
@@ -0,0 +1,25 @@
+define xinetd::file(
+  $ensure = 'present',
+  $source = 'absent',
+  $content = 'absent',
+){
+  include ::xinetd
+  file{"/etc/xinetd.d/${name}":
+    ensure => $ensure,
+    require => Package['xinetd'],
+    notify => Service['xinetd'],
+    owner => root, group => 0, mode => 0644;
+  }
+
+  if $ensure == 'present' {
+    if $source != 'absent' {
+      File["/etc/xinetd.d/${name}"]{
+        source => $source,
+      }
+    } else {
+      File["/etc/xinetd.d/${name}"]{
+        content => $content,
+      }
+    }
+  }
+}
